0959db04715196d756253d34d10e3adc01676088
Seoya0512 <seoya.lee512@gmail.com>
2026-03-04 18:53:37 +0900

    rollback: code-rollback

--- app/core/redis.py
+++ app/core/redis.py
@@ -6,7 +6,7 @@ _client: Redis | None = None
 
 async def init_redis(url: str) -> None:
     global _client
-    _client = aioredis.from_url(url, decode_responses=True, ssl_cert_reqs=None)
+    _client = aioredis.from_url(url, decode_responses=True)
 
 
 async def close_redis() -> None:
